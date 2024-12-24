70e6410c5caa326a291218586eeb234035ce6413
Alexandre Djioev <adjioev@vervio.com.au>
2024-12-24 22:23:16 +0400

    adding .gitingore

--- server/api/ping.get.ts
+++ server/api/ping.get.ts
@@ -1,5 +1,6 @@
 export default defineEventHandler((event) => {
+    console.log("This is a test log");
     return {
-        hello: 'hello from debvelop branch!'
+        hello: 'hello from debvelop branch 1232!'
     }
 })
